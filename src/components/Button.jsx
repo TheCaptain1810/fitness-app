9808c322d4b07bb78e4d76dc115ddfa9579ea202
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-08-07 10:59:25 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button(props) {
     const { text, func } = props;
 
   return (
-    <button onClick={func} className="px-8 py-4 mx-auto rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+    <button onClick={func} className="px-8 py-4 mx-auto rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200 mb-10">
         <p>{text}</p>
     </button>
   )
